f7ff9fe106265dfcd8c68990e9f0387fdce09844
knowckx <knowckx@foxmail.com>
2021-07-20 17:17:40 +0800

    add week range

--- timerange.go
+++ timerange.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+var (
+	BackOffset = -1 * time.Millisecond
+)
+
 type TimeRange struct {
 	Start time.Time
 	End   time.Time
@@ -18,7 +22,7 @@ func (s TimeRange) String() string {
 // GetDayRange 1945-10-10 12:12:12 --> [1945-10-10 00:00:00, 1945-10-10 23:59:59]
 func GetDayRange(t time.Time) *TimeRange {
 	tiStart := GetDayStart(t)
-	tiEnd := tiStart.AddDate(0, 0, 1).Add(-1 * time.Second)
+	tiEnd := tiStart.AddDate(0, 0, 1).Add(BackOffset)
 	tr := &TimeRange{
 		tiStart, tiEnd,
 	}
@@ -31,12 +35,26 @@ func GetDayStart(t time.Time) time.Time {
 	return tiStart
 }
 
+// GetWeekRange 1945-10-10 12:12:12 --> [1945-10-08 00:00:00, 1945-10-14 23:59:59]
+func GetWeekRange(in time.Time) *TimeRange {
+	weekDay := int(in.Weekday())
+	dayFix := 1 // monday is the first day of week
+	weekDay -= dayFix
+	dayStart := GetDayStart(in)
+	tiStart := dayStart.AddDate(0, 0, -1*weekDay)
+	tiEnd := tiStart.AddDate(0, 0, 7).Add(BackOffset)
+	tr := &TimeRange{
+		tiStart, tiEnd,
+	}
+	return tr
+}
+
 // GetMonthRange 1945-10-10 12:12:12 --> [1945-10-01 00:00:00, 1945-10-30 23:59:59]
 func GetMonthRange(ti time.Time) *TimeRange {
 	today := GetDayStart(ti)
 	dayOffset := ti.Day()*-1 + 1
 	start := today.AddDate(0, 0, dayOffset)
-	end := start.AddDate(0, 1, 0).Add(-1 * time.Second)
+	end := start.AddDate(0, 1, 0).Add(BackOffset)
 	tr := TimeRange{
 		start, end,
 	}
--- timerange_test.go
+++ timerange_test.go
@@ -6,19 +6,24 @@ import (
 	"time"
 )
 
-func Test_GetLastMonthRange(t *testing.T) {
+func Test_GetDayRange_Now(t *testing.T) {
+	ti := time.Now()
+	tr := GetDayRange(ti)
+	fmt.Println(tr)
+}
+
+func Test_GetWeekRange_Now(t *testing.T) {
+	ti := time.Now()
+	tr := GetWeekRange(ti)
+	fmt.Println(tr)
+}
+func Test_GetLastMonthRange_Now(t *testing.T) {
 	tr := GetLastMonthRange()
 	fmt.Println(tr)
 }
 
-func Test_GetMonthRange(t *testing.T) {
+func Test_GetMonthRange_Now(t *testing.T) {
 	ti := time.Now()
 	tr := GetMonthRange(ti)
-	fmt.Println(tr)
-}
-
-func Test_GetDayRange(t *testing.T) {
-	ti := time.Now()
-	tr := GetDayRange(ti)
 	fmt.Println(tr)
 }
